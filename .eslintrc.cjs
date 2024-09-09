f6c5a0088e2727d1a7e24c8ea09e2c9e8791e86c
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2024-09-09 14:22:13 +0300

    Main structure  (#2)
    * Added main structure, styled with grid.
    
    * Separated components and styles in different folders.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,47 @@
-/** @type { import('eslint').Linter.Config } */
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'airbnb',
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: [
+      "./tsconfig.json",
+      "./tsconfig.app.json",
+      "./tsconfig.node.json"
+    ]
+  },
+  plugins: ["react", "@typescript-eslint", "prettier", "jsx-a11y"],
+  rules: {
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function"
+      }
     ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: [
-            './tsconfig.json',
-            './tsconfig.app.json',
-            './tsconfig.node.json',
-        ],
-    },
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    rules: {
-        'react/react-in-jsx-scope': 0,
-    },
+    "func-style": ["error", "expression"],
+    "prefer-arrow-callback": "error",
+    "no-confusing-arrow": "error",
+    "react/jsx-no-bind": [
+      "warn",
+      {
+        allowArrowFunctions: true,
+        ignoreRefs: true
+      }
+    ],
+    "react/react-in-jsx-scope": "off"
+  }
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-    return <div>Social network</div>;
-}
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import Content from './components/Content/Content';
+
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app_Wrapper}>
+      <Header />
+      <Navigation />
+      <Content />
+    </div>
+  );
+};
 
 export default App;
