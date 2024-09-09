9d690a97d5d1f26ca0548766dd1643a65887d418
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-09-09 19:36:04 +0300

    Added basic routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
-import Content from './components/Content/Content';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 import styles from './App.module.css';
 
 const App = () => {
   return (
-    <div className={styles.app_Wrapper}>
+    <div className={styles.app_wrapper}>
       <Header />
       <Navigation />
-      <Content />
+      <main className={styles.content_wrapper}>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="dialogs" element={<Dialogs />} />
+          <Route path="music" element={<Music />} />
+          <Route path="news" element={<News />} />
+          <Route path="settings" element={<Settings />} />
+        </Routes>
+      </main>
     </div>
   );
 };
