edd62489f68dc22e72f095db0158239e89e8273e
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-09-09 13:47:14 +0300

    Added main structure, styled with grid.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-    return <div>Social network</div>;
-}
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Content from './components/Content';
+
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app_Wrapper}>
+      <Header />
+      <Navigation />
+      <Content />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+  return (
+    <nav className={styles.navigation}>
+      <ul>
+        <li>
+          <a href="#">Profile</a>
+        </li>
+        <li>
+          <a href="#">Messages</a>
+        </li>
+        <li>
+          <a href="#">News</a>
+        </li>
+        <li>
+          <a href="#">Music</a>
+        </li>
+        <li>
+          <a href="#">Settings</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
