3903135eba042136bfa3a7b5ffa6adcb88debdf9
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2024-09-10 17:38:02 +0300

    Added nested routing and Home component. (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
+import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 
-import styles from './App.module.css';
-
 const App = () => {
   return (
-    <div className={styles.app_wrapper}>
-      <Header />
-      <Navigation />
-      <main className={styles.content_wrapper}>
-        <Routes>
-          <Route path="/" element={<Profile />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="dialogs/*" element={<Dialogs />} />
-          <Route path="music" element={<Music />} />
-          <Route path="news" element={<News />} />
-          <Route path="settings" element={<Settings />} />
-        </Routes>
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<Profile />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="dialogs/*" element={<Dialogs />} />
+        <Route path="music" element={<Music />} />
+        <Route path="news" element={<News />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
   );
 };
 
