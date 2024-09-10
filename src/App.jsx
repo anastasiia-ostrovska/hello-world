36ab0cb8865a96e39e6ab68691cc0650173efd3c
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-09-10 18:14:21 +0300

    Replaced mocked data to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,16 @@ import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
+const App = ({ posts, dialogs, messages }) => {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
-        <Route index element={<Profile />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="dialogs/*" element={<Dialogs />} />
+        <Route index element={<Profile posts={posts} />} />
+        <Route path="profile" element={<Profile posts={posts} />} />
+        <Route
+          path="dialogs/*"
+          element={<Dialogs dialogs={dialogs} messages={messages} />}
+        />
         <Route path="music" element={<Music />} />
         <Route path="news" element={<News />} />
         <Route path="settings" element={<Settings />} />
