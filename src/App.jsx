b274407b25c1e63b8eb18ed7335d6d5207d24198
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-10-09 17:51:48 +0300

    feature : Add lazy loading for all main pages in routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import LinearPreloader from '@components/common/prealoaders/LinearPreloader';
 
-import Components from '@/components-presentation/Components';
-import Home from './components/Home/Home';
-import Dialogs from './components/Dialogs/Dialogs';
-import Saved from './components/Saved/Saved';
-import News from './components/News/News';
-import Settings from './components/Settings/Settings';
-import Users from './components/Users/Users';
-import ProfileContainer from './components/Profile/ProfileContainer';
+const Components = lazy(() => import('@/components-presentation/Components'));
+
+const Home = lazy(() => import('@components/Home/Home'));
+const Dialogs = lazy(() => import('@components/Dialogs/Dialogs'));
+const Saved = lazy(() => import('@components/Saved/Saved'));
+const News = lazy(() => import('@components/News/News'));
+const Settings = lazy(() => import('@components/Settings/Settings'));
+const Users = lazy(() => import('@components/Users/Users'));
+const ProfileContainer = lazy(
+  () => import('@components/Profile/ProfileContainer')
+);
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<ProfileContainer />} />
-        <Route path="profile/:userId?" element={<ProfileContainer />} />
-        <Route path="dialogs/*" element={<Dialogs />} />
-        <Route path="users" element={<Users />} />
-        <Route path="saved" element={<Saved />} />
-        <Route path="news" element={<News />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
-      <Route path="/components" element={<Components />} />
-    </Routes>
+    <Suspense fallback={<LinearPreloader />}>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<ProfileContainer />} />
+          <Route path="profile/:userId?" element={<ProfileContainer />} />
+          <Route path="dialogs/*" element={<Dialogs />} />
+          <Route path="users" element={<Users />} />
+          <Route path="saved" element={<Saved />} />
+          <Route path="news" element={<News />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="/components" element={<Components />} />
+      </Routes>
+    </Suspense>
   );
 };
 
