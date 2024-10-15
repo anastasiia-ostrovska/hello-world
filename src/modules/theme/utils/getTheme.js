7a0c4fea9d31781275b4f94f6dbd102f5bc9c2cb
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-10-15 14:01:22 +0300

    feature : Refactored FollowButton component. Added button's borders to theme.

--- src/modules/theme/utils/getTheme.js
+++ src/modules/theme/utils/getTheme.js
@@ -2,6 +2,15 @@ import { createTheme } from '@mui/material';
 
 const getTheme = (mode) => {
   return createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: '2rem',
+          },
+        },
+      },
+    },
     palette: {
       mode,
     },
