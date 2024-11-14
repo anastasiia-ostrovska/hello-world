4d71a2cb384a66408f5a618adf2636391142c5d9
Anastasiia Ostrovska <anastasiyakov99@gmail.com>
2024-11-14 14:00:01 +0200

    feature : Created pagination. Added custom hook for storing currentPage instead of redux store.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,28 @@
-import { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import { useGetUsersQuery } from '@/modules/users/store/usersApi';
-import {
-  setCurrentPage,
-  selectUsersCountOnPage,
-  selectCurrentPage,
-} from '@/modules/users/store/usersReducer';
+import useCurrentPage from '@/modules/users/hooks/useCurrentPage';
+import getPagesCount from '@/modules/users/utils/getPagesCount';
 import LinearPreloader from '@components/common/prealoaders/LinearPreloader';
 import UserCardsList from '@/modules/users/ui/containers/UserCardsList';
-
-import styles from './Users.module.css';
+import Pagination from '@mui/material/Pagination';
 
 const Users = () => {
-  const dispatch = useDispatch();
-  const usersCount = useSelector(selectUsersCountOnPage);
-  const currentPage = useSelector(selectCurrentPage);
+  const [usersPerPageCount] = useState(8);
+  const [currentPage, handlePageChange] = useCurrentPage(1);
 
   const { data: users, isLoading } = useGetUsersQuery({
-    usersCount,
+    usersPerPageCount,
     currentPage,
   });
 
-  const handlePageChange = useCallback(
-    (page) => {
-      dispatch(setCurrentPage(page));
-    },
-    [dispatch]
-  );
-
   if (!isLoading) {
-    const { totalCount } = users;
-    const pagesCount = Math.ceil((totalCount - 26600) / usersCount);
-    const pages = Array.from({ length: pagesCount }, (_, index) => index + 1);
-
     return (
       <div>
-        <div>
-          {pages.map((page) => (
-            <button
-              key={page}
-              type="button"
-              className={page === currentPage ? styles.active : ''}
-              onClick={() => handlePageChange(page)}
-              style={{ fontSize: '2rem' }}
-              disabled={isLoading}
-            >
-              {page}
-            </button>
-          ))}
-        </div>
+        <Pagination
+          count={getPagesCount(users.totalCount, usersPerPageCount)}
+          page={currentPage}
+          onChange={handlePageChange}
+        />
         <UserCardsList users={users.items} />
       </div>
     );
