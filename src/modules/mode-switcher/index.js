97648d6720379fdbce17c630df8d6ba110da2091
Anastasiia <anastasiyakov99@gmail.com>
2024-12-30 16:15:32 +0200

    refactoring : replaced old version of app to separate temporary directory. Removed unused code.

--- src/modules/mode-switcher/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ModeSwitcher } from './ui/containers/ModeSwitcher';
---
+++ src/modules/theme/ui/containers/ModeSwitcher.jsx
@@ -0,0 +1,19 @@
+import {
+  selectIsLightMode,
+  toggleMode,
+} from '@/modules/theme/store/themeSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import SwitchButton from '../components/SwitchButton';
+
+const ModeSwitcher = () => {
+  const dispatch = useDispatch();
+  const isLightMode = useSelector(selectIsLightMode);
+
+  const handleSwitchMode = () => {
+    dispatch(toggleMode());
+  };
+
+  return <SwitchButton isLightMode={isLightMode} onClick={handleSwitchMode} />;
+};
+
+export default ModeSwitcher;
