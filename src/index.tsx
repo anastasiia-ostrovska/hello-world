924c811061fdf41623c0577def6ed8e397f25714
Anastasiia <anastasiyakov99@gmail.com>
2025-01-07 15:42:27 +0200

    refactoring : refactored index.

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import store from './store/store';
 
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeWrapper>
