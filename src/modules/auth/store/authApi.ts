00190664a6e5dd734c5cc37a4db3c5ff8d0c502d
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-09 20:56:54 +0200

    refactoring : added interface AuthMeResponse.

--- src/modules/auth/store/authApi.ts
+++ src/modules/auth/store/authApi.ts
@@ -1,9 +1,20 @@
 import { AUTH_ME } from '@/shared/constants/API-endpoints';
 import baseAPI from '@/redux/base-API';
 
+interface AuthData {
+  id: number;
+  email: string;
+  login: string;
+}
+interface AuthMeResponse {
+  data: AuthData;
+  resultCode: number;
+  messages: string[];
+}
+
 const authApi = baseAPI.injectEndpoints({
   endpoints: (builder) => ({
-    getAuthData: builder.query({
+    getAuthData: builder.query<AuthMeResponse, void>({
       query: () => AUTH_ME,
     }),
   }),
