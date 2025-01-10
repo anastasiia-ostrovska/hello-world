ff6040c10096b77edba22fecfed42cbea61cc8c5
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-10 14:30:33 +0200

    refactoring : added types to profileApi.

--- src/modules/profile/store/profileApi.ts
+++ src/modules/profile/store/profileApi.ts
@@ -1,9 +1,10 @@
+import { ProfileDataResponse } from '@/modules/profile/types';
 import { PROFILE } from '@/shared/constants/API-endpoints';
 import baseAPI from '@/redux/base-API';
 
 const profileApi = baseAPI.injectEndpoints({
   endpoints: (builder) => ({
-    getUserProfile: builder.query({
+    getUserProfile: builder.query<ProfileDataResponse, number>({
       query: (userId) => `${PROFILE}/${userId}`,
     }),
   }),
