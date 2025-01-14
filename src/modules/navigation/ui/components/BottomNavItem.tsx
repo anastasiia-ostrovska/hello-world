b69dbba74e97d86183ad4ee789f9f0ac91c1f364
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-14 15:07:30 +0200

    refactoring : added types to BottomNavItem component.

--- src/modules/navigation/ui/components/BottomNavItem.tsx
+++ src/modules/navigation/ui/components/BottomNavItem.tsx
@@ -1,7 +1,14 @@
+import { NavigationItemProps } from '@/modules/navigation/types';
 import { memo } from 'react';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
-const BottomNavItem = ({ label, icon, path, color, onClick }) => {
+const BottomNavItem = ({
+  label,
+  icon,
+  path,
+  color,
+  onClick,
+}: NavigationItemProps) => {
   return (
     <BottomNavigationAction
       aria-label={label}
