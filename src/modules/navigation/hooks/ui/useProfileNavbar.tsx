1f9a17b9f0060a09e6cb9929273b572706a8a3e9
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-14 15:41:13 +0200

    refactoring : added types to useProfileNavbar hook.

---
+++ src/modules/navigation/hooks/ui/useProfileNavbar.tsx
@@ -0,0 +1,56 @@
+import { Path } from '@/modules/navigation/types';
+import { MouseEvent, useCallback, useEffect, useState } from 'react';
+import useNavigateToPath from '@/modules/navigation/hooks/handlers/useNavigateToPath';
+
+type AnchorEl = HTMLElement | null;
+type MenuBtnClickHandler = (event: MouseEvent<HTMLElement>) => void;
+type MenuCloseHandler = () => void;
+type MenuItemClickHandler = (path: Path) => void;
+
+type ProfileNavbarChildrenProps = {
+  anchorEl: AnchorEl;
+  isMenuOpen: boolean;
+  handleMenuButtonClick: MenuBtnClickHandler;
+  handleMenuItemClick: MenuItemClickHandler;
+  handleMenuClose: MenuCloseHandler;
+};
+
+const useProfileNavbar = (): ProfileNavbarChildrenProps => {
+  const [anchorEl, setAnchorEl] = useState<AnchorEl>(null);
+  const [pendingPath, setPendingPath] = useState<Path | ''>('');
+  const isMenuOpen = Boolean(anchorEl);
+  const handleNavigate = useNavigateToPath();
+
+  useEffect(() => {
+    if (!isMenuOpen && pendingPath) {
+      setPendingPath('');
+      handleNavigate(pendingPath);
+    }
+  }, [handleNavigate, isMenuOpen, pendingPath]);
+
+  const handleMenuButtonClick: MenuBtnClickHandler = useCallback((event) => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+
+  const handleMenuClose: MenuCloseHandler = useCallback(() => {
+    setAnchorEl(null);
+  }, []);
+
+  const handleMenuItemClick: MenuItemClickHandler = useCallback(
+    (path) => {
+      handleMenuClose();
+      setPendingPath(path);
+    },
+    [handleMenuClose]
+  );
+
+  return {
+    anchorEl,
+    isMenuOpen,
+    handleMenuButtonClick,
+    handleMenuItemClick,
+    handleMenuClose,
+  };
+};
+
+export default useProfileNavbar;
