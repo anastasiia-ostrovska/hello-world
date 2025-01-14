4d201beee9bb799ed6116475de0df09198f5a809
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-14 15:15:49 +0200

    refactoring : added types to ProfileNavItem component.

--- src/modules/navigation/ui/components/ProfileNavItem.tsx
+++ src/modules/navigation/ui/components/ProfileNavItem.tsx
@@ -1,8 +1,15 @@
+import { NavigationItemProps } from '@/modules/navigation/types';
 import { memo } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 
-const ProfileNavItem = ({ label, icon, path, color, onClick }) => {
+const ProfileNavItem = ({
+  label,
+  icon,
+  path,
+  color,
+  onClick,
+}: NavigationItemProps) => {
   return (
     <MenuItem onClick={() => onClick(path)} sx={{ color }}>
       <ListItemIcon sx={{ color }}>{icon}</ListItemIcon>
