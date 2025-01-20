375f46507849a56f0f77b624237623d97c8c2fc6
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-20 20:31:56 +0200

    feature : log-out DELETE request.

--- src/modules/auth/store/authApi.ts
+++ src/modules/auth/store/authApi.ts
@@ -1,10 +1,16 @@
+import { ApiResponseTemplate } from '@/shared/types/response-data';
 import { AuthMeResponse, LogInData, LogInResponse } from '@/modules/auth/types';
 import { baseAPI } from '@/redux';
-import { POST } from '@/shared/constants/query-methods';
+import { POST, DELETE } from '@/shared/constants/query-methods';
 import { LOGIN, AUTH_ME } from '@/shared/constants/query-endpoints';
 
 const authApi = baseAPI.injectEndpoints({
   endpoints: (builder) => ({
+    getAuthData: builder.query<AuthMeResponse, void>({
+      query: () => AUTH_ME,
+      providesTags: ['Auth'],
+    }),
+
     logIn: builder.mutation<LogInResponse, LogInData>({
       query: (logInData) => ({
         method: POST,
@@ -14,9 +20,12 @@ const authApi = baseAPI.injectEndpoints({
       invalidatesTags: ['Auth'],
     }),
 
-    getAuthData: builder.query<AuthMeResponse, void>({
-      query: () => AUTH_ME,
-      providesTags: ['Auth'],
+    logOut: builder.mutation<ApiResponseTemplate, void>({
+      query: () => ({
+        method: DELETE,
+        url: LOGIN,
+      }),
+      invalidatesTags: ['Auth'],
     }),
   }),
 });
