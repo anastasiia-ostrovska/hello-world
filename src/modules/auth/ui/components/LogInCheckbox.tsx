7470b6a8655a21b0be65bf8b32dd11c5f9698008
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-01-24 16:23:25 +0200

    feature : login form register (react-hook-form) (#79)
    * feature : installed react-hook-form and its devtools deps.
    
    * feature : added DevTool component.
    
    * feature : registered LogInInput on react-hook-form.
    
    * feature : registered LogInCheckbox on react-hook-form.
    
    * feature : added submit handler (temporarily logging in console).

--- src/modules/auth/ui/components/LogInCheckbox.tsx
+++ src/modules/auth/ui/components/LogInCheckbox.tsx
@@ -1,28 +1,39 @@
-import { LogInInputProps } from '@/modules/auth/types';
+import { Controller, useFormContext } from 'react-hook-form';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 
-const LogInCheckbox = ({
-  id,
-  label,
-  helperText,
-  sx = {},
-}: Omit<LogInInputProps, 'type' | 'error'>) => {
+interface LogInInputProps {
+  name: string;
+  label: string;
+  helperText: string;
+}
+
+const LogInCheckbox = ({ name, label, helperText }: LogInInputProps) => {
+  const { control } = useFormContext();
+
   return (
-    <FormControl sx={{ ...sx }}>
-      <FormControlLabel
-        control={
-          <Checkbox
-            defaultChecked
-            size="small"
-            aria-describedby={`${id}-helper-text`}
+    <FormControl>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <FormControlLabel
+            control={
+              <Checkbox
+                {...field}
+                checked={!!field.value}
+                onChange={(event) => field.onChange(event.target.checked)}
+                size="small"
+                aria-describedby={`${name}-helper-text`}
+              />
+            }
+            label={label}
           />
-        }
-        label={label}
+        )}
       />
-      <FormHelperText id={`${id}-helper-text`}>{helperText}</FormHelperText>
+      <FormHelperText id={`${name}-helper-text`}>{helperText}</FormHelperText>
     </FormControl>
   );
 };
--- src/modules/auth/ui/containers/LogInForm.tsx
+++ src/modules/auth/ui/containers/LogInForm.tsx
@@ -1,3 +1,6 @@
+import { FormProvider, useForm } from 'react-hook-form';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { DevTool } from '@hookform/devtools';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
@@ -5,37 +8,57 @@ import LogInInput from '@/modules/auth/ui/components/LogInInput';
 import LogInCheckbox from '@/modules/auth/ui/components/LogInCheckbox';
 import Button from '@mui/material/Button';
 
+interface LogInData {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
+
 const LogInForm = () => {
+  const methods = useForm<LogInData>({
+    defaultValues: { email: '', password: '', rememberMe: true },
+  });
+  const { control, handleSubmit } = methods;
+
+  const onSubmit = (data: LogInData) => {
+    console.log(data);
+  };
+
   return (
-    <Paper elevation={2} sx={{ p: 2 }}>
-      <Stack component="form" noValidate spacing={4} sx={{ width: '100%' }}>
-        <Typography variant="h5">Log in</Typography>
-        <Stack spacing={2}>
-          <LogInInput
-            id="email"
-            label="Email"
-            type="email"
-            error={false}
-            helperText="e.g., example@mail.com"
-          />
-          <LogInInput
-            id="password"
-            label="Password"
-            type="password"
-            error={false}
-            helperText="8+ chars, uppercase, number and symbol"
-          />
-          <LogInCheckbox
-            id="remember-me"
-            label="Remember me"
-            helperText="Stay logged in on this device"
-          />
+    <FormProvider {...methods}>
+      <Paper elevation={2} sx={{ p: 2 }}>
+        <Stack
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          spacing={4}
+          sx={{ width: '100%' }}
+        >
+          <Typography variant="h5">Log in</Typography>
+          <Stack spacing={2}>
+            <LogInInput
+              name="email"
+              label="Email"
+              helperText="e.g., example@mail.com"
+            />
+            <LogInInput
+              name="password"
+              label="Password"
+              helperText="8+ chars, uppercase, number and symbol"
+            />
+            <LogInCheckbox
+              name="rememberMe"
+              label="Remember me"
+              helperText="Stay logged in on this device"
+            />
+          </Stack>
+          <Button type="submit" variant="contained" sx={{ mt: 8 }}>
+            Log in
+          </Button>
         </Stack>
-        <Button type="submit" variant="contained" sx={{ mt: 8 }}>
-          Log in
-        </Button>
-      </Stack>
-    </Paper>
+        <DevTool control={control} /> {/* set up the dev tool */}
+      </Paper>
+    </FormProvider>
   );
 };
 
