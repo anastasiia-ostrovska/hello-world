959d5a5a8707ca099329b6a7106849af36bab0b0
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-01-28 17:55:28 +0200

    feature : added ControlledCheckbox component to login form.

--- src/modules/auth/ui/components/LogInCheckbox.tsx
+++ src/modules/auth/ui/components/LogInCheckbox.tsx
@@ -1,40 +1,19 @@
-import { Controller, useFormContext } from 'react-hook-form';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
+import { ControlledCheckbox } from '@/modules/forms';
 
 interface LogInInputProps {
   name: string;
   label: string;
-  helperText: string;
+  helperText?: string;
 }
 
-const LogInCheckbox = ({ name, label, helperText }: LogInInputProps) => {
-  const { control } = useFormContext();
-
+const LogInCheckbox = ({ name, label, helperText = '' }: LogInInputProps) => {
   return (
-    <FormControl>
-      <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-          <FormControlLabel
-            control={
-              <Checkbox
-                {...field}
-                checked={!!field.value}
-                onChange={(event) => field.onChange(event.target.checked)}
-                size="small"
-                aria-describedby={`${name}-helper-text`}
-              />
-            }
-            label={label}
-          />
-        )}
-      />
-      <FormHelperText id={`${name}-helper-text`}>{helperText}</FormHelperText>
-    </FormControl>
+    <ControlledCheckbox
+      name={name}
+      label={label}
+      helperText={helperText}
+      size="small"
+    />
   );
 };
 
