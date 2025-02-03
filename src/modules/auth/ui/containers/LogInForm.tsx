17b58117d3885c544306ea182f83c429ff457cff
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-03 17:29:27 +0200

    feature : guest login data (#91)
    * refactoring : SectionWrapper props.
    
    * refactoring : changed minLength validation (from 1 to 14).
    
    * feature : created section with guest data filling button.

--- src/modules/auth/ui/containers/LogInForm.tsx
+++ src/modules/auth/ui/containers/LogInForm.tsx
@@ -2,9 +2,10 @@ import { FormProvider, useForm } from 'react-hook-form';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { DevTool } from '@hookform/devtools';
 import { useMemo } from 'react';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
+import SectionWrapper from '@/layout/ui/components/SectionWrapper';
 import LogInEmailInput from '@/modules/auth/ui/components/LogInEmailInput';
 import LogInPasswordInput from '@/modules/auth/ui/components/LogInPasswordInput';
 import LogInCheckbox from '@/modules/auth/ui/components/LogInCheckbox';
@@ -25,6 +26,7 @@ const LogInForm = () => {
   const {
     control,
     handleSubmit,
+    setValue,
     formState: { dirtyFields, errors, isSubmitting },
   } = methods;
 
@@ -39,26 +41,64 @@ const LogInForm = () => {
     console.log(data);
   };
 
+  const handleFillGuestData = () => {
+    const encodedPassword = encodeURIComponent(
+      import.meta.env.VITE_GUEST_PASSWORD
+    );
+
+    setValue('email', import.meta.env.VITE_GUEST_EMAIL, {
+      shouldValidate: true,
+      shouldDirty: true,
+    });
+    setValue('password', encodedPassword, {
+      shouldValidate: true,
+      shouldDirty: true,
+    });
+  };
+
   return (
     <FormProvider {...methods}>
-      <Paper elevation={2} sx={{ width: 300, p: 2 }}>
-        <Stack
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-          spacing={4}
-          sx={{ width: '100%' }}
+      <Stack spacing={1} sx={{ maxWidth: 350, width: '100%' }}>
+        <Typography variant="h5" sx={{ alignSelf: 'center' }}>
+          Log in
+        </Typography>
+        <SectionWrapper
+          sx={{ flexDirection: 'column', gap: 1, alignItems: 'center' }}
         >
-          <Typography variant="h5">Log in</Typography>
-          <Stack spacing={2}>
-            <LogInEmailInput />
-            <LogInPasswordInput />
-            <LogInCheckbox />
+          <Typography
+            variant="caption"
+            color="success"
+            sx={{ textAlign: 'center' }}
+          >
+            * Please, use the Guest credentials to log in.
+          </Typography>
+          <Chip
+            component="button"
+            label="Fill with Guest Data"
+            color="success"
+            variant="outlined"
+            onClick={handleFillGuestData}
+            sx={{ cursor: 'pointer' }}
+          />
+        </SectionWrapper>
+        <SectionWrapper>
+          <Stack
+            component="form"
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            spacing={4}
+            sx={{ width: '100%' }}
+          >
+            <Stack spacing={2}>
+              <LogInEmailInput />
+              <LogInPasswordInput />
+              <LogInCheckbox />
+            </Stack>
+            <LogInSubmitButton isDisabled={isDisabled} />
           </Stack>
-          <LogInSubmitButton isDisabled={isDisabled} />
-        </Stack>
-        <DevTool control={control} /> {/* set up the dev tool */}
-      </Paper>
+          <DevTool control={control} /> {/* set up the dev tool */}
+        </SectionWrapper>
+      </Stack>
     </FormProvider>
   );
 };
