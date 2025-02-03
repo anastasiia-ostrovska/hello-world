1f3a0a8bb2ebd7378de79d47c7b77c07797ced5b
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-03 16:29:30 +0200

    refactoring : SectionWrapper props.

--- src/layout/ui/components/SectionWrapper.tsx
+++ src/layout/ui/components/SectionWrapper.tsx
@@ -1,17 +1,16 @@
+import { PaperProps } from '@mui/material';
 import { ReactNode } from 'react';
-import { Styles } from '@/shared/types/mui-props';
 import Paper from '@mui/material/Paper';
 
-interface SectionWrapperProps {
+interface SectionWrapperProps extends PaperProps {
   children: ReactNode;
-  sx?: Styles;
 }
 
-const SectionWrapper = ({ children, sx = {} }: SectionWrapperProps) => {
+const SectionWrapper = ({ children, ...props }: SectionWrapperProps) => {
   return (
     <Paper
       component="section"
-      sx={{ display: 'flex', p: { xs: 1, sm: 2, md: 3 }, ...sx }}
+      sx={{ display: 'flex', p: { xs: 1, sm: 2, md: 3 }, ...props }}
     >
       {children}
     </Paper>
