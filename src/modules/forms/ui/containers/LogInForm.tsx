63ec66a47dc6036d56f6703531e83b0e2b125804
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-05 16:58:30 +0200

    feature : submit button (#101)
    * refactoring : created reusable SubmitButton component. Removed unused LogInSubmitButton.
    
    * feature : added default prop variant='contained' for MuiButton in custom theme.
    
    * refactoring : added SubmitButton to LogInForm.
    
    * refactoring : removed wrapper from form component and replaced it to parent component.

--- src/modules/forms/ui/containers/LogInForm.tsx
+++ src/modules/forms/ui/containers/LogInForm.tsx
@@ -1,10 +1,9 @@
 import { BaseSyntheticEvent } from 'react';
 import Stack from '@mui/material/Stack';
-import SectionWrapper from '@/layout/ui/components/SectionWrapper';
 import LogInEmailInput from '@/modules/forms/ui/components/LogInEmailInput';
 import LogInPasswordInput from '@/modules/forms/ui/components/LogInPasswordInput';
 import LogInCheckbox from '@/modules/forms/ui/components/LogInCheckbox';
-import LogInSubmitButton from '@/modules/forms/ui/components/LogInSubmitButton';
+import SubmitButton from '@/modules/forms/ui/components/SubmitButton';
 
 interface LogInFormProps {
   onSubmit: (e?: BaseSyntheticEvent) => Promise<void>;
@@ -13,22 +12,20 @@ interface LogInFormProps {
 
 const LogInForm = ({ onSubmit, isButtonDisabled }: LogInFormProps) => {
   return (
-    <SectionWrapper>
-      <Stack
-        component="form"
-        onSubmit={onSubmit}
-        noValidate
-        spacing={4}
-        sx={{ width: '100%' }}
-      >
-        <Stack spacing={2}>
-          <LogInEmailInput name="email" />
-          <LogInPasswordInput name="password" />
-          <LogInCheckbox name="rememberMe" />
-        </Stack>
-        <LogInSubmitButton isDisabled={isButtonDisabled} />
+    <Stack
+      component="form"
+      onSubmit={onSubmit}
+      noValidate
+      spacing={3}
+      sx={{ width: '100%' }}
+    >
+      <Stack spacing={2}>
+        <LogInEmailInput name="email" />
+        <LogInPasswordInput name="password" />
+        <LogInCheckbox name="rememberMe" />
       </Stack>
-    </SectionWrapper>
+      <SubmitButton disabled={isButtonDisabled}>Log in</SubmitButton>
+    </Stack>
   );
 };
 
