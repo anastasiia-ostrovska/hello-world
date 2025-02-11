7ba133a192e299bd7e68eff5b3bfda14157c0684
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-11 16:11:36 +0200

    feature: AppInitLoader component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from '@/layout';
-import { ROUTES_CONFIG, ROUTES } from '@/routing';
+import { ROUTES, ROUTES_CONFIG } from '@/routing';
+import LogIn from '@/pages/LogIn';
 import Home from '@/pages/Home';
 
-const LogIn = lazy(() => import('@/pages/LogIn'));
 const Components = lazy(
   () => import('@/_old-version/components-presentation/Components')
 );
