a7bf6cb5ae7b91c08d94837a6e18752d4585c28c
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-11 20:50:00 +0200

    feature : protected routes (#106)
    * feature: ProtectedRoute component.
    
    * feature: added ProtectedRoute component to App.

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,29 @@ import { Layout } from '@/layout';
 import { ROUTES, ROUTES_CONFIG } from '@/routing';
 import LogIn from '@/pages/LogIn';
 import Home from '@/pages/Home';
+import ProtectedRoute from '@/shared/components/ProtectedRoute';
 
 const Components = lazy(
   () => import('@/_old-version/components-presentation/Components')
 );
 
-const App = () => {
+interface AppProps {
+  isAuth: boolean;
+}
+
+const App = ({ isAuth }: AppProps) => {
   return (
     <Routes>
       <Route path={ROUTES.LOGIN} element={<LogIn />} />
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        {ROUTES_CONFIG.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
+      <Route element={<ProtectedRoute isAllowed={isAuth} />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {ROUTES_CONFIG.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
       </Route>
-      <Route path="/components" element={<Components />} />
+      <Route path="/components" element={<Components />} />;
     </Routes>
   );
 };
---
+++ src/shared/components/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { ROUTES } from '@/routing';
+
+interface ProtectedRouteProps {
+  isAllowed: boolean;
+  redirectPath?: string;
+  children?: ReactNode;
+}
+
+const ProtectedRoute = ({
+  isAllowed,
+  redirectPath = ROUTES.LOGIN,
+  children,
+}: ProtectedRouteProps) => {
+  const location = useLocation();
+
+  if (!isAllowed) {
+    return <Navigate to={redirectPath} replace state={{ from: location }} />;
+  }
+
+  return children || <Outlet />;
+};
+
+export default ProtectedRoute;
