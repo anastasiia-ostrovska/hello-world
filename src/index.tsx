05c16c15bea90c9c293b2dac4c6efcf6b76ffd5a
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-12 16:19:14 +0200

    refactoring: root index file.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ThemeWrapper } from '@/modules/theme';
 import { store } from '@/redux';
-import App from './App';
+import { ThemeWrapper } from '@/modules/theme';
+import AppInitializer from './app/AppInitializer';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <BrowserRouter>
         <ThemeWrapper>
-          <App />
+          <AppInitializer />
         </ThemeWrapper>
       </BrowserRouter>
     </Provider>
