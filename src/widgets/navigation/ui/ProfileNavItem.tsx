67634fe1c84aa8d90521c129eb01a832a4cb4f65
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-17 13:34:23 +0200

    refactoring: ProfileNavItem component in widgets layer.

---
+++ src/widgets/navigation/ui/ProfileNavItem.tsx
@@ -0,0 +1,16 @@
+import { memo } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { Link as RouterLink } from 'react-router-dom';
+import { NavigationItemProps } from '../model/types';
+
+const ProfileNavItem = ({ label, icon, path, color }: NavigationItemProps) => {
+  return (
+    <MenuItem component={RouterLink} to={path} sx={{ color }}>
+      <ListItemIcon sx={{ color }}>{icon}</ListItemIcon>
+      {label}
+    </MenuItem>
+  );
+};
+
+export default memo(ProfileNavItem);
