ba97cfa8de3b7fadf724af8fb3f127d54e9616d2
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-19 15:47:20 +0200

    refactoring: useAvatar.ts and UserAvatar.tsx.

---
+++ src/features/users/ui/UserAvatar.tsx
@@ -0,0 +1,33 @@
+import { Styles } from '@/shared/mui';
+import useAvatar from '@/modules/users/hooks/ui/useAvatar';
+import Avatar from '@mui/material/Avatar';
+import { Photo, User } from '../model/types';
+
+export interface UserAvatarProps {
+  userName: User['name'];
+  src: Photo;
+  size: number;
+  sx?: Styles;
+}
+
+const UserAvatar = ({ userName, src, size, sx = {} }: UserAvatarProps) => {
+  const { bgColor, initials } = useAvatar(userName);
+
+  return (
+    <Avatar
+      alt={userName || 'Avatar is loading...'}
+      src={src || ''}
+      sx={{
+        width: size,
+        height: size,
+        fontSize: size * 0.5,
+        backgroundColor: userName ? bgColor : 'default',
+        ...sx,
+      }}
+    >
+      {userName && initials}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
