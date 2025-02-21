b0241a56740a0dd00f9049fe7503ded0eb818632
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-21 12:24:48 +0200

    refactoring : loaders (#125)
    * refactoring: replaced SkeletonText and SkeletonWrapper components into ui segment of shared layer.
    
    * refactoring: fixed imports of skeleton loaders.
    
    * refactoring: removed loaders from modules.

--- src/modules/loaders/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SkeletonText } from './ui/components/SkeletonText';
-export { default as SkeletonWrapper } from './ui/components/SkeletonWrapper';
--- src/modules/loaders/ui/components/SkeletonText.tsx
+++
@@ -1,8 +0,0 @@
-import { LoaderProps } from '@/modules/loaders/types';
-import Skeleton from '@mui/material/Skeleton';
-
-const SkeletonText = ({ isLoading, element }: LoaderProps) => {
-  return isLoading ? <Skeleton /> : element || null;
-};
-
-export default SkeletonText;
