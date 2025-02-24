0fe912c1a1450c49516e42c3df7030579e34794f
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-24 13:57:16 +0200

    refactoring: moved AuthMeResponse interface into types.ts.

--- src/shared/api/model/authApi.ts
+++ src/shared/api/model/authApi.ts
@@ -1,16 +1,7 @@
-import { User } from '@/shared/user';
-import { ApiResponseTemplate } from './types';
+import { AuthMeResponse } from './types';
 import { AUTH_ME } from '../config/endpoints';
 import * as TAGS from '../config/invalidation-tags';
 import baseAPI from './baseApi';
-
-export interface AuthData {
-  id: User['id'];
-  login: string;
-  email: string;
-}
-
-type AuthMeResponse = ApiResponseTemplate<AuthData>;
 
 const authApi = baseAPI.injectEndpoints({
   endpoints: (builder) => ({
