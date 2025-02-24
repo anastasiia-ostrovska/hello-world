bcd74f30f688671481c67aa55f8f5046f2d525d2
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-24 16:38:45 +0200

    feature: used Form component in LogInForm.tsx.

--- src/modules/forms/ui/containers/LogInForm.tsx
+++ src/modules/forms/ui/containers/LogInForm.tsx
@@ -1,4 +1,5 @@
 import { BaseSyntheticEvent } from 'react';
+import { Form } from '@/shared/forms';
 import Stack from '@mui/material/Stack';
 import LogInEmailInput from '@/modules/forms/ui/components/LogInEmailInput';
 import LogInPasswordInput from '@/modules/forms/ui/components/LogInPasswordInput';
@@ -17,20 +18,14 @@ const LogInForm = ({
   isLogInRequestLoading,
 }: LogInFormProps) => {
   return (
-    <Stack
-      component="form"
-      onSubmit={onSubmit}
-      noValidate
-      spacing={3}
-      sx={{ width: '100%' }}
-    >
+    <Form onSubmit={onSubmit} spacing={3}>
       <Stack spacing={2}>
         <LogInEmailInput name="email" disabled={isLogInRequestLoading} />
         <LogInPasswordInput name="password" disabled={isLogInRequestLoading} />
         <LogInCheckbox name="rememberMe" disabled={isLogInRequestLoading} />
       </Stack>
       <SubmitButton disabled={isButtonDisabled}>Log in</SubmitButton>
-    </Stack>
+    </Form>
   );
 };
 
--- src/shared/forms/index.ts
+++ src/shared/forms/index.ts
@@ -1,4 +1,5 @@
 export * from './model/validators';
 
+export { default as Form } from './ui/Form';
 export { default as ControlledCheckbox } from './ui/ControlledCheckbox';
 export { default as ControlledTextField } from './ui/ControlledTextField';
