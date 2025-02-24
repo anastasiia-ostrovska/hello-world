f7ef06e11f5b8d9b43361c79b9e7124ca2253b2b
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-24 18:21:29 +0200

    refactoring : login UI (#134)
    * refactoring: LogOutButton.tsx. Changed imports.
    
    * feature: created reusable Form component in shared.
    
    * feature: used Form component in LogInForm.tsx.
    
    * feature: LogInFormLayout.tsx.
    
    * refactoring: LogInForm.tsx
    
    * refactoring: Moved reusable SubmitButton into shared layer.
    
    * refactoring: Fixed imports. Removed form from modules/auth and modules/forms
    
    * refactoring: created input-items.ts config.
    
    * refactoring: added enum for inputs name attribute.
    
    * refactoring: added layout for inputs and used it in LogInForm.tsx.
    
    * refactoring: removed unused modules/auth and modules/forms.

--- src/features/login/model/types.ts
+++ src/features/login/model/types.ts
@@ -1,5 +1,11 @@
+export enum LogInInput {
+  Email = 'email',
+  Password = 'password',
+  RememberMe = 'rememberMe',
+}
+
 export interface LogInData {
-  email: string;
-  password: string;
-  rememberMe: boolean;
+  [LogInInput.Email]: string;
+  [LogInInput.Password]: string;
+  [LogInInput.RememberMe]: boolean;
 }
--- src/modules/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LogOutButton } from './ui/containers/LogOutButton';
-export { default as LogInForm } from './ui/containers/LogInFormContainer';
