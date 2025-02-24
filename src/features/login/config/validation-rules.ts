f4d09e48b7ae9c26cb3acccfe67f126ac33952c6
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-24 17:32:50 +0200

    refactoring: login validation configs.

---
+++ src/features/login/config/validation-rules.ts
@@ -0,0 +1,36 @@
+import {
+  containsDigit,
+  containsSpecialCharacter,
+  containsUppercase,
+  fieldIsRequired,
+  minLength14,
+  validEmailFormat,
+  whitespacesForbidden,
+} from '@/shared/forms';
+import {
+  emailValidationMessage,
+  passwordValidationMessage,
+} from './validation-messages';
+
+export const emailValidationRules = {
+  fieldIsRequired: fieldIsRequired(emailValidationMessage.required),
+  whitespacesForbidden: whitespacesForbidden(
+    emailValidationMessage.whitespacesForbidden
+  ),
+  validEmailFormat: validEmailFormat(emailValidationMessage.invalidEmailFormat),
+};
+
+export const passwordValidationRules = {
+  fieldIsRequired: fieldIsRequired(passwordValidationMessage.required),
+  whitespacesForbidden: whitespacesForbidden(
+    passwordValidationMessage.whitespacesForbidden
+  ),
+  uppercaseRequired: containsUppercase(
+    passwordValidationMessage.uppercaseRequired
+  ),
+  digitRequired: containsDigit(passwordValidationMessage.digitRequired),
+  specialCharacterRequired: containsSpecialCharacter(
+    passwordValidationMessage.specialCharacterRequired
+  ),
+  minLength14: minLength14(passwordValidationMessage.minLength14),
+};
