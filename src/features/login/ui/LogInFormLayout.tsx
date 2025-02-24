c6db0b1178bee6654347c249c47e974f83a4cf1d
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-24 17:07:55 +0200

    feature: LogInFormLayout.tsx.

---
+++ src/features/login/ui/LogInFormLayout.tsx
@@ -0,0 +1,60 @@
+import { ReactElement, ReactNode } from 'react';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import SectionWrapper from '@/shared/ui/SectionWrapper';
+
+interface LogInFormLayoutProps {
+  heading: string;
+  guestData: ReactElement;
+  children: ReactNode;
+}
+
+export const LogInFormLayout = ({
+  heading,
+  guestData,
+  children,
+}: LogInFormLayoutProps) => {
+  return (
+    <Stack spacing={1} sx={{ maxWidth: 300, width: '100%' }}>
+      <Typography variant="h5" component="h1" sx={{ alignSelf: 'center' }}>
+        {heading}
+      </Typography>
+      <SectionWrapper
+        sx={{
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 1,
+        }}
+      >
+        {guestData}
+      </SectionWrapper>
+      <SectionWrapper>{children}</SectionWrapper>
+    </Stack>
+  );
+};
+
+interface GuestDataLayoutProps {
+  text: string;
+  button: ReactElement;
+}
+
+export const GuestDataLayout = ({ text, button }: GuestDataLayoutProps) => {
+  return (
+    <SectionWrapper
+      sx={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 1,
+      }}
+    >
+      <Typography
+        variant="caption"
+        color="success"
+        sx={{ textAlign: 'center' }}
+      >
+        {text}
+      </Typography>
+      {button}
+    </SectionWrapper>
+  );
+};
