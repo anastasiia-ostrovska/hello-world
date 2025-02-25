906472c0862a4b76fc0733a6ab2c076549c4ae85
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-02-25 12:13:41 +0200

    refactoring: ProfileMenuButton.tsx and ProfileAvatar.tsx components.

---
+++ src/features/profile/ui/ProfileAvatar.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from 'react';
+import { User } from '@/shared/user';
+import { SkeletonWrapper } from '@/shared/ui/SkeletonLoaders';
+import { UserAvatar } from '@/features/users';
+import { useGetUserProfileQuery } from '@/features/profile/api/profileApi';
+import { skipToken } from '@reduxjs/toolkit/query';
+
+interface ProfileAvatarLayoutProps {
+  avatar: ReactElement;
+  isLoading: boolean;
+}
+
+const ProfileAvatarLayout = ({
+  avatar,
+  isLoading,
+}: ProfileAvatarLayoutProps) => {
+  return (
+    <SkeletonWrapper
+      variant="circular"
+      isLoading={isLoading}
+      element={avatar}
+    />
+  );
+};
+
+interface ProfileAvatarProps {
+  userId: User['id'] | undefined;
+  size: number;
+}
+
+const ProfileAvatar = ({ userId, size }: ProfileAvatarProps) => {
+  const { data, isLoading } = useGetUserProfileQuery(userId ?? skipToken);
+
+  return (
+    <ProfileAvatarLayout
+      avatar={
+        <UserAvatar
+          userName={data?.fullName || ''}
+          src={data?.photos?.small || null}
+          size={size}
+        />
+      }
+      isLoading={isLoading || !userId}
+    />
+  );
+};
+
+export default ProfileAvatar;
