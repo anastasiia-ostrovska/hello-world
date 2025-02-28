999688d9b89c754b554bd875719c29501de838fc
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-02-28 14:39:10 +0200

    feature : not found route (#141)
    * feature: added not-found-icon.
    
    * fix: colors of not-found-icon.
    
    * feature: NotFoundPage.tsx. with layout.
    
    * feature: added route in Routing.tsx.

---
+++ src/pages/not-found/ui/NotFoundPageLayout.tsx
@@ -0,0 +1,39 @@
+import { ReactElement } from 'react';
+import Stack from '@mui/material/Stack';
+
+interface NotFoundPageLayoutProps {
+  icon: ReactElement;
+  helperText: ReactElement;
+  goHomeButton: ReactElement;
+}
+
+const NotFoundPageLayout = ({
+  icon,
+  helperText,
+  goHomeButton,
+}: NotFoundPageLayoutProps) => {
+  return (
+    <Stack
+      sx={{
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Stack
+        spacing={5}
+        sx={{
+          minWidth: '250px',
+          width: '35vw',
+          fontSize: { xs: '14px', md: '18px', xl: '24px' },
+        }}
+      >
+        {icon}
+        {helperText}
+        {goHomeButton}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default NotFoundPageLayout;
