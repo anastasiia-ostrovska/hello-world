f4db66dc361f3ee4c31f954d332f09686f62fdcb
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-03-04 20:26:15 +0200

    feature: created token-storage-key.ts and tokenStorageHandlers.ts to store and remove token from localStorage.

---
+++ src/shared/api/model/tokenStorageHandlers.ts
@@ -0,0 +1,10 @@
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from '@shared/webStorage';
+import { TOKEN_STORAGE_KEY } from '../config/token-storage-key';
+
+export const setStoredToken = setToLocalStorage(TOKEN_STORAGE_KEY);
+export const getStoredToken = getFromLocalStorage(TOKEN_STORAGE_KEY);
+export const removeStoredToken = removeFromLocalStorage(TOKEN_STORAGE_KEY);
--- src/shared/webStorage/model/getFromLocalStorage.ts
+++
@@ -1,5 +0,0 @@
-const getFromLocalStorage = (key: string) => (): string | null => {
-  return localStorage.getItem(key);
-};
-
-export default getFromLocalStorage;
