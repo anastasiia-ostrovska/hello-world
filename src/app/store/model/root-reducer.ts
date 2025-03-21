918b2284ae53f5de09031159754fc76c6394e758
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-03-21 15:35:34 +0200

    fix: changed error feature to notification feature.

--- src/app/store/model/root-reducer.ts
+++ src/app/store/model/root-reducer.ts
@@ -1,13 +1,13 @@
 import { combineSlices } from '@reduxjs/toolkit';
 import { authSlice, baseAPI } from '@shared/api';
-import { errorSlice } from '@features/error';
+import { notificationSlice } from '@features/notification';
 import { themeSlice } from '@features/theme';
 import { usersSlice } from '@features/users';
 
 const rootReducer = combineSlices(
   baseAPI,
   // mockAPI,
-  errorSlice,
+  notificationSlice,
   authSlice,
   themeSlice,
   usersSlice
