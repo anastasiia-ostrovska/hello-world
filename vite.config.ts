8eaba544cb06a73ae55a95c3b061906260d73e7f
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-03-27 18:22:39 +0200

    feature: added @entitias path alias.

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@widgets': path.resolve(__dirname, './src/widgets'),
       '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
       '@shared': path.resolve(__dirname, './src/shared'),
       '@': path.resolve(__dirname, './src'),
     },
