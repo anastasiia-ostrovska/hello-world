99d80fd6f989c9f53682b2aae263d7d9f3ecb896
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-03-28 16:01:48 +0200

    feature: NotificationActions (#159)
    * feature: NotificationActions feature.
    
    * feature: CloseAllNotificationsButton component.
    
    * feature: RemoveNotificationIconButton component.
    
    * refactoring: used controls from notification entity in NotificationActions.tsx.

--- src/entities/notification/index.ts
+++ src/entities/notification/index.ts
@@ -8,3 +8,5 @@ export {
 
 export { EmbeddedNotification } from './ui/EmbeddedNotification';
 export { PopupNotification } from './ui/PopupNotification';
+export { RemoveNotificationIconButton } from './ui/RemoveNotificationIconButton';
+export { CloseAllNotificationsButton } from './ui/CloseAllNotificationsButton';
---
+++ src/features/notification/ui/NotificationActions.tsx
@@ -0,0 +1,30 @@
+import Tooltip from '@mui/material/Tooltip';
+import Stack from '@mui/material/Stack';
+import { useAppDispatch } from '@shared/redux';
+import {
+  clearAllNotifications,
+  CloseAllNotificationsButton,
+  removeNotification,
+  RemoveNotificationIconButton,
+} from '@entities/notification';
+
+export const NotificationActions = ({ id }: { id: string }) => {
+  const dispatch = useAppDispatch();
+
+  const handleRemoveNotification = () => {
+    dispatch(removeNotification(id));
+  };
+
+  const handleClearAllNotification = () => {
+    dispatch(clearAllNotifications());
+  };
+
+  return (
+    <Stack direction="row" sx={{ alignSelf: 'end' }}>
+      <Tooltip title="Dismiss all notifications">
+        <CloseAllNotificationsButton onClick={handleClearAllNotification} />
+      </Tooltip>
+      <RemoveNotificationIconButton onClick={handleRemoveNotification} />
+    </Stack>
+  );
+};
