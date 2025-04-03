6446de25003c4b4db1db68193ed44b42b49570d0
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-03 13:35:58 +0300

    feature: added ModeSwitcher to the TopNavbar.

--- src/features/mode-switcher/ui/ModeSwitcher.tsx
+++ src/features/mode-switcher/ui/ModeSwitcher.tsx
@@ -1,6 +1,7 @@
 import IconButton from '@mui/material/IconButton';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { useTheme } from '@mui/material/styles';
 import { useModeSwitcher } from '../model/useModeSwitcher';
 
 interface ModeIconProps {
@@ -12,12 +13,16 @@ const ModeIcon = ({ isDarkMode }: ModeIconProps) => {
 };
 
 const ModeSwitcher = () => {
+  const { palette } = useTheme();
   const { isDarkMode, nextMode, handleToggleMode } = useModeSwitcher();
 
   return (
     <IconButton
       aria-label={`switch to ${nextMode} mode`}
       onClick={handleToggleMode}
+      sx={{
+        color: palette.text.secondary,
+      }}
     >
       <ModeIcon isDarkMode={isDarkMode} />
     </IconButton>
