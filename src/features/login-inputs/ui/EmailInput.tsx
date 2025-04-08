2b3b632b081c1b9af15d64f833c8aa7818b923df
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-08 11:18:05 +0300

    refactoring : split login inputs into separated files.

--- src/features/login-inputs/ui/EmailInput.tsx
+++ src/features/login-inputs/ui/EmailInput.tsx
@@ -4,7 +4,3 @@ import { inputs } from '../config/input-items';
 export const EmailInput = ({ isDisabled }: { isDisabled: boolean }) => {
   return <ControlledTextField {...inputs.email} disabled={isDisabled} />;
 };
-
-export const PasswordInput = ({ isDisabled }: { isDisabled: boolean }) => {
-  return <ControlledTextField {...inputs.password} disabled={isDisabled} />;
-};
