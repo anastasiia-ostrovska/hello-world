50911eaa320c8fdf4096f3d780215b4998da9c79
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-09 11:47:49 +0300

    feature : buildOptionalQueryParams.ts function to create a query string with optional parameters. Added to userApi.ts

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,4 +3,6 @@ export * as ENDPOINTS from './config/endpoints';
 export * as METHODS from './config/methods';
 export * as TAGS from './config/invalidation-tags';
 
+export { buildOptionalQueryParams } from './lib/buildOptionalQueryParams';
+
 export { default as baseAPI } from './baseApi';
