589a2680215c923a6e97cc8519d7f43c4aec75ac
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-10 16:56:40 +0300

    refactoring : usersList request with types.

--- src/entities/user/model/usersSlice.ts
+++ src/entities/user/model/usersSlice.ts
@@ -1,33 +1,33 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { UsersQueryParams } from './types';
+import { UsersListFilters } from './types';
 
-const initialState: UsersQueryParams = {
-  usersPerPage: 12,
-  currentPage: 1,
-  searchedUser: undefined,
-  isFollowedByMe: undefined,
+export const initialUsersListFilters: UsersListFilters = {
+  count: 12,
+  page: 1,
+  search: '',
+  following: false,
 };
 
 export const usersSlice = createSlice({
-  name: 'usersQueryParams',
-  initialState,
+  name: 'usersListFilters',
+  initialState: initialUsersListFilters,
   selectors: {
     selectUsersQueryParams: (state) => state,
-    selectUsersPerPage: (state) => state.usersPerPage,
-    selectCurrentPage: (state) => state.currentPage,
+    selectUsersPerPage: (state) => state.count,
+    selectCurrentPage: (state) => state.page,
   },
   reducers: {
     setUsersPerPage: (state, action: PayloadAction<number>) => {
-      state.usersPerPage = action.payload;
+      state.count = action.payload;
     },
     setCurrentPage: (state, action: PayloadAction<number>) => {
-      state.currentPage = action.payload;
+      state.page = action.payload;
     },
     setSearchedUser: (state, action: PayloadAction<string>) => {
-      state.searchedUser = action.payload;
+      state.search = action.payload;
     },
     setIsFollowedByMe: (state, action: PayloadAction<boolean>) => {
-      state.isFollowedByMe = action.payload;
+      state.following = action.payload;
     },
   },
 });
