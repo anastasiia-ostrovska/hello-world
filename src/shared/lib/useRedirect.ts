e5554b7a43a7ba9661e2f3f896bb9bf4c1042e08
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-12 17:31:06 +0300

    refactoring : shared/lib.

--- src/shared/lib/useRedirect.ts
+++ src/shared/lib/useRedirect.ts
@@ -4,11 +4,13 @@ interface UseRedirectReturn {
   redirectedFrom: string;
 }
 
-const useRedirect = ({ altRoute }: { altRoute: string }): UseRedirectReturn => {
+export const useRedirect = ({
+  altRoute,
+}: {
+  altRoute: string;
+}): UseRedirectReturn => {
   const location = useLocation();
   const redirectedFrom: string = location.state?.from?.pathname || altRoute;
 
   return { redirectedFrom };
 };
-
-export default useRedirect;
