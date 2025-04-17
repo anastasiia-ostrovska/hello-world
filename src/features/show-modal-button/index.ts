320db15653341bc1749658f6232f2bf2a7dfc44a
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-17 14:49:28 +0300

    feature : ShowModalButton.tsx component.

---
+++ src/features/show-modal-button/index.ts
@@ -0,0 +1,1 @@
+export { default as ShowModalButton } from './ui/ShowModalButton';
---
+++ src/features/show-modal-button/ui/ShowModalButton.tsx
@@ -0,0 +1,51 @@
+import { ReactNode, useState } from 'react';
+import Box, { BoxProps } from '@mui/material/Box';
+import Modal from '@shared/ui/Modal';
+
+interface ShowModalButtonProps extends BoxProps {
+  showModalButton: ReactNode;
+  modalTitle: ReactNode;
+  modalContent: ReactNode;
+  modalActions?: ReactNode;
+  withTitleDivider?: boolean;
+  withActionsDivider?: boolean;
+  onButtonClick?: () => void;
+}
+
+const ShowModalButton = ({
+  showModalButton,
+  modalTitle,
+  modalContent,
+  modalActions = null,
+  withTitleDivider = false,
+  withActionsDivider = false,
+  onButtonClick,
+  ...boxProps
+}: ShowModalButtonProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleDialogOpen = () => {
+    setIsOpen(true);
+    onButtonClick?.();
+  };
+  const handleDialogClose = () => setIsOpen(false);
+
+  return (
+    <>
+      <Box {...boxProps} onClick={handleDialogOpen}>
+        {showModalButton}
+      </Box>
+      <Modal
+        isOpen={isOpen}
+        title={modalTitle}
+        content={modalContent}
+        actions={modalActions}
+        withTitleDivider={withTitleDivider}
+        withActionsDivider={withActionsDivider}
+        onClose={handleDialogClose}
+      />
+    </>
+  );
+};
+
+export default ShowModalButton;
