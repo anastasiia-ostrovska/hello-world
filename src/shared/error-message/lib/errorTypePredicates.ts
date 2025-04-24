f1386f8790d2ceb457f8a40db8e124b7f2384e66
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-24 11:07:45 +0300

    feature: IsErrorTypeInErrorParams function to reveal if 'errorType' exists in error response.

--- src/shared/error-message/lib/errorTypePredicates.ts
+++ src/shared/error-message/lib/errorTypePredicates.ts
@@ -1,4 +1,5 @@
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import { Error } from '../model/types';
 
 /**
  * Type predicate to narrow an unknown error-message to `FetchBaseQueryError`
@@ -20,5 +21,23 @@ export const isSerializedError = (
     error != null &&
     'message' in error &&
     typeof (error as any).message === 'string'
+  );
+};
+
+interface IsErrorTypeInErrorParams {
+  error: unknown;
+  errorType: Error;
+}
+
+export const isErrorTypeInError = ({
+  error,
+  errorType,
+}: IsErrorTypeInErrorParams): boolean => {
+  return (
+    isFetchBaseQueryError(error) &&
+    !!error.data &&
+    typeof error.data === 'object' &&
+    'errorType' in error.data &&
+    error.data.errorType === errorType
   );
 };
