f722a990f75577832a523d98d5b18fa8a1c0a548
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-04-30 18:35:42 +0300

    feature: useUserWithFallback hook with types.

--- src/entities/user/model/useUserWithFallback.ts
+++ src/entities/user/model/useUserWithFallback.ts
@@ -1,4 +1,4 @@
-import { skipToken } from '@reduxjs/toolkit/query';
+import { useMemo } from 'react';
 import { useUserByIdQuery } from '../api/userApi';
 import { generateFakeUsers } from '../lib/generateFakeUsers';
 import { UserWithInfo } from './types';
@@ -10,16 +10,22 @@ interface UserWithFallback {
   isLoading: boolean;
 }
 
+/**
+ * A function that retrieves a user by their ID or provides a fallback user if the actual user cannot be fetched.
+ *
+ * @param {Object} params - The parameters required to fetch the user.
+ * @param {string} params.userId - The unique identifier for the user to be fetched.
+ * @returns {UserWithFallback} An object containing user data along with additional status flags for the fetch operation.
+ */
+
 export const useUserWithFallback = ({
   userId,
 }: {
   userId: string;
 }): UserWithFallback => {
-  const { data, error, isError, isLoading } = useUserByIdQuery(
-    userId ?? skipToken
-  );
-  const fakeUser = generateFakeUsers(1)[0];
-  const user = data?.data || fakeUser;
+  const { data, error, isError, isLoading } = useUserByIdQuery(userId);
+  const fakeUser = useMemo(() => generateFakeUsers(1)[0], []);
+  const user = data?.data ?? fakeUser;
 
   return { user, error, isError, isLoading };
 };
