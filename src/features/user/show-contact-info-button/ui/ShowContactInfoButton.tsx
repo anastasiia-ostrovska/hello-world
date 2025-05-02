077ee0e21fdd9f37dee50eb082c20ac598709208
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-05-02 17:04:35 +0300

    ShowContactInfoButton: refactored props.

--- src/features/user/show-contact-info-button/ui/ShowContactInfoButton.tsx
+++ src/features/user/show-contact-info-button/ui/ShowContactInfoButton.tsx
@@ -1,12 +1,25 @@
-import { Button } from '@mui/material';
+import { Button, ButtonProps } from '@mui/material';
 import { SkeletonWrapper } from '@shared/ui';
 
-const ShowContactInfoButton = ({ isLoading }: { isLoading: boolean }) => {
+interface ShowContactInfoButtonProps extends ButtonProps {
+  isLoading: boolean;
+}
+
+const ShowContactInfoButton = ({
+  isLoading,
+  sx = {},
+  ...buttonProps
+}: ShowContactInfoButtonProps) => {
   return (
     <SkeletonWrapper
       isLoading={isLoading}
       element={
-        <Button variant="text" size="small" sx={{ ml: '-5px' }}>
+        <Button
+          variant="text"
+          size="small"
+          sx={{ ml: '-5px', ...sx }}
+          {...buttonProps}
+        >
           Contact info
         </Button>
       }
