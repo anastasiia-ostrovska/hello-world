337215ca553d46c4f4df38b7418aa50d40dda715
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-05-07 19:52:41 +0300

    getDefaultInputsValues: added JSDoc description.

--- src/widgets/profile-info-editor/lib/getDefaultInputsValues.ts
+++ src/widgets/profile-info-editor/lib/getDefaultInputsValues.ts
@@ -5,6 +5,19 @@ import {
   UserInfoItemLabel,
 } from '../model/types';
 
+/**
+ * Generates and returns the default input values for a profile information editor.
+ *
+ * The function extracts relevant user information and maps it to predefined labels
+ * for the profile editor. It ensures that each field is populated with a default
+ * empty string ('') if the corresponding user data is unavailable.
+ *
+ * @param {UserWithInfo} user - The user object containing profile and contact information.
+ * @returns {ProfileInfoEditorInputs} An object representing the default input values
+ * for the profile information editor, with empty values as fallback where specific
+ * user data is missing.
+ */
+
 export const getDefaultInputsValues = (
   user: UserWithInfo
 ): ProfileInfoEditorInputs => {
