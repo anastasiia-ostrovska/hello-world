904ca8cba612b6d7c07b2c97f61f576f8df464c6
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-05-14 20:39:22 +0300

    withCapitalLetter: exported from public api of shared/lib.

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,5 +1,7 @@
 export * from './localStorageHandlers';
 export * from './typePredicates';
 
+export { withCapitalLetter } from './withCapitalLetter';
+
 export { useMediaQueryState } from './useMediaQueryState';
 export { useRedirect } from './useRedirect';
