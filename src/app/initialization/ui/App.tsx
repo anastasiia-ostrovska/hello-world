6d2ef864311de3c24d09ab8d91eb7b5b3b46044c
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-05-16 18:00:03 +0300

    app: moved ModalController and PopupNotificationWithControls into AppLayout.

--- src/app/initialization/ui/App.tsx
+++ src/app/initialization/ui/App.tsx
@@ -1,21 +1,11 @@
 import { Routing } from '@app/routing';
-import { PopupNotificationWithControls } from '@widgets/notification';
-import { ModalController } from '@features/modal-controller';
 import useInitializeApp from '../model/useInitializeApp';
 import AppLoader from './AppLoader';
 
 const App = () => {
   const { userId, isInitialized } = useInitializeApp();
 
-  return isInitialized ? (
-    <>
-      <Routing userId={userId} />
-      <ModalController />
-      <PopupNotificationWithControls />
-    </>
-  ) : (
-    <AppLoader />
-  );
+  return isInitialized ? <Routing userId={userId} /> : <AppLoader />;
 };
 
 export default App;
